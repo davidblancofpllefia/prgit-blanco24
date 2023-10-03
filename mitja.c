c88e646de2080926f0dc9bf5e9b7eccf1344d55c
daw2-blanco24 <blancofernandezdavid@fpllefia.com>
2023-10-03 17:37:46 +0200

    prgit-blanco24 v1

---
+++ mitja.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+	float suma=0;
+	float mitja;
+	float nou_valor;
+	int numero_repeticions=0;
+	char consulta='s';
+	
+	while(consulta=='s') {
+		printf("blanco24 Dòna la següent dada: ");
+		scanf("%f",&nou_valor);
+		suma=suma+nou_valor;
+		numero_repeticions=numero_repeticions+1;
+		printf("blanco24 Vols continuar (s/n): ");
+		scanf("%s",&consulta); 
+	}
+	mitja=suma/numero_repeticions;
+	printf("blanco24 La mitja és: %.2f\n",mitja);	
+	return 0;
+}
